5af2dc0af596aee5f44ffeeda5bb149d156f2d28
HaiNinh <luongvuhaininh123@gmail.com>
2023-09-12 21:27:54 +0700

    fix error getRoot()

--- app/src/main/java/com/example/iot_app/MainActivity.java
+++ app/src/main/java/com/example/iot_app/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
         replaceFragment(new HomeFragment());
 
         binding.bottomNav.setOnItemSelectedListener(item -> {
