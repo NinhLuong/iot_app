985bde7cb1dd55233a60d0ee27b487d6f10010c1
NhuY <ledangnhuy16@gmail.com>
2023-11-22 19:14:19 +0700

    no message

---
+++ app/src/main/java/com/example/iot_app/LampFragment.java
@@ -0,0 +1,36 @@
+package com.example.iot_app;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+public class LampFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_lamp, container, false);
+
+        Toolbar toolbar = rootView.findViewById(R.id.toolbar);
+        ((AppCompatActivity) requireActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity) requireActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ((AppCompatActivity) requireActivity()).getSupportActionBar().setTitle("Desk lamp");
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requireActivity().onBackPressed();
+            }
+        });
+
+        return rootView;
+    }
+}
--- app/src/main/java/com/example/iot_app/WebFragment.java
+++ app/src/main/java/com/example/iot_app/WebFragment.java
@@ -1,64 +1,59 @@
 package com.example.iot_app;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link WebFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 public class WebFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public WebFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment WebFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static WebFragment newInstance(String param1, String param2) {
-        WebFragment fragment = new WebFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private Button changeLampButton;
+    private Button changeAirButton;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_web, container, false);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_web, container, false);
+        changeLampButton = rootView.findViewById(R.id.change_lamp);
+        changeAirButton = rootView.findViewById(R.id.change_ari);
+
+        changeLampButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Chuyển sang fragment_lamp và đè hoàn toàn lên fragment cũ
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                LampFragment fragmentLamp = new LampFragment();
+                fragmentTransaction.replace(R.id.fragment_container, fragmentLamp);
+                fragmentTransaction.commit();
+
+                // Ẩn hai button
+                changeLampButton.setVisibility(View.GONE);
+                changeAirButton.setVisibility(View.GONE);
+            }
+        });
+
+        changeAirButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Chuyển sang fragment_air và đè hoàn toàn lên fragment cũ
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                AirFragment fragmentAir = new AirFragment();
+                fragmentTransaction.replace(R.id.fragment_container, fragmentAir);
+                fragmentTransaction.commit();
+
+                // Ẩn hai button
+                changeLampButton.setVisibility(View.GONE);
+                changeAirButton.setVisibility(View.GONE);
+            }
+        });
+
+        return rootView;
     }
 }
