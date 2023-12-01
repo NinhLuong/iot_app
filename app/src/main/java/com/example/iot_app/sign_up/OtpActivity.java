b5b672856c4836e01a4b875201af875c7456da88
Ninh Luong Vu Hai <intern.lvhninh@tma.com.vn>
2023-12-01 14:16:35 +0700

    update code activity_otp
    update code activity_otp

--- app/src/main/java/com/example/iot_app/sign_up/OtpActivity.java
+++ app/src/main/java/com/example/iot_app/sign_up/OtpActivity.java
@@ -3,14 +3,23 @@ package com.example.iot_app.sign_up;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.iot_app.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthProvider;
 
 public class OtpActivity extends AppCompatActivity {
+    Long timeoutSeconds = 60L;
+    String verificationCode;
+    PhoneAuthProvider.ForceResendingToken  resendingToken;
+    TextView resendOtpTextView;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp);
     }
+
 }
