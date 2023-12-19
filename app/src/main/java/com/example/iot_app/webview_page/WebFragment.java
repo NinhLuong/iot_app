5d1c39d89f15a8a2fcfbc5ff3dfa34b819fa5108
NhuY <ledangnhuy16@gmail.com>
2023-12-19 11:18:26 +0700

    no message

--- app/src/main/java/com/example/iot_app/webview_page/WebFragment.java
+++ app/src/main/java/com/example/iot_app/webview_page/WebFragment.java
@@ -10,14 +10,14 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.example.iot_app.FanFragment;
 import com.example.iot_app.R;
 import com.example.iot_app.device.AirFragment;
 import com.example.iot_app.device.LampFragment;
 
 public class WebFragment extends Fragment {
 
-    private Button changeLampButton;
-    private Button changeAirButton;
+    private Button changeLampButton, changeAirButton, changeFanButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -25,11 +25,11 @@ public class WebFragment extends Fragment {
 
         changeLampButton = rootView.findViewById(R.id.change_lamp);
         changeAirButton = rootView.findViewById(R.id.change_ari);
+        changeFanButton = rootView.findViewById(R.id.change_fan);
 
         changeLampButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Chuyển sang fragment_lamp và đè hoàn toàn lên fragment cũ
                 FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                 LampFragment fragmentLamp = new LampFragment();
@@ -39,13 +39,13 @@ public class WebFragment extends Fragment {
                 // Ẩn hai button
                 changeLampButton.setVisibility(View.GONE);
                 changeAirButton.setVisibility(View.GONE);
+                changeFanButton.setVisibility(View.GONE);
             }
         });
 
         changeAirButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Chuyển sang fragment_air và đè hoàn toàn lên fragment cũ
                 FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                 AirFragment fragmentAir = new AirFragment();
@@ -55,6 +55,23 @@ public class WebFragment extends Fragment {
                 // Ẩn hai button
                 changeLampButton.setVisibility(View.GONE);
                 changeAirButton.setVisibility(View.GONE);
+                changeFanButton.setVisibility(View.GONE);
+            }
+        });
+
+        changeFanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                FanFragment fragmentFan = new FanFragment();
+                fragmentTransaction.replace(R.id.fragment_container, fragmentFan);
+                fragmentTransaction.commit();
+
+                // Ẩn hai button
+                changeLampButton.setVisibility(View.GONE);
+                changeAirButton.setVisibility(View.GONE);
+                changeFanButton.setVisibility(View.GONE);
             }
         });
 
